a417e66d657e953c1341306c590bef759d931342
yuhsuan <frorgr@hotmail.com>
2019-09-11 21:22:10 +1000

    Updates

--- precache-manifest.7497754f14e9473d88bfacb6d375d7c2.js
+++ precache-manifest.7497754f14e9473d88bfacb6d375d7c2.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d47e15002be7e721ba4b85e9fbd4eb6d",
+    "revision": "092b5565b885f1588b4561f312bfb725",
     "url": "/61mid-autumn/index.html"
   },
   {
-    "revision": "52bb3de648e10d96d184",
-    "url": "/61mid-autumn/static/css/main.1444294e.chunk.css"
+    "revision": "605725ae902335f21187",
+    "url": "/61mid-autumn/static/css/main.56fd0f3f.chunk.css"
   },
   {
-    "revision": "1f54b5dfcabfa110e70b",
-    "url": "/61mid-autumn/static/js/2.3193c5e1.chunk.js"
+    "revision": "ef126667e747f71e677f",
+    "url": "/61mid-autumn/static/js/2.20182f68.chunk.js"
   },
   {
-    "revision": "52bb3de648e10d96d184",
-    "url": "/61mid-autumn/static/js/main.8a280988.chunk.js"
+    "revision": "605725ae902335f21187",
+    "url": "/61mid-autumn/static/js/main.66114561.chunk.js"
   },
   {
     "revision": "513b7783706bc0661a17",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/61mid-autumn/precache-manifest.bd6c000363c83e22a317dc66ed0dc4bc.js"
+  "/61mid-autumn/precache-manifest.7497754f14e9473d88bfacb6d375d7c2.js"
 );
 
 self.addEventListener('message', (event) => {
